fd79fd0805a4dd12b8db808ff74444f8720f6858
Ina <valentinacalderon466@gmail.com>
2024-05-06 12:02:57 -0400

    Agregando tipos de datos

---
+++ TIPOSDATOS.py
@@ -0,0 +1,6 @@
+#STRINGS
+
+institucion = "Universidad de Los Lagos"
+asignatura = 'Programación'
+descripcion = """asignatura de primer semestre
+de la carrera de Ingeniería Civil en Informática"""
