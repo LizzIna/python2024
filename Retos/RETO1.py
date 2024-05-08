3f13276e504f1e02766dae14977bd7c192d4a8d9
Ina <valentinacalderon466@gmail.com>
2024-05-08 16:48:44 -0400

    subiendo reto 1

---
+++ Retos/RETO1.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre")
+edad = int(input("Ingrese su edad"))
+edadmas = edad + 20
+
+print(f"Mi nombre es {nombre} , tengo {edad} años y en 20 años mas tendre {edadmas}")
