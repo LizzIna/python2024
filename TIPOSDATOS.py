a345164094920970480a541a3b4f10c812c673ec
Ina <valentinacalderon466@gmail.com>
2024-05-08 16:07:17 -0400

    Se agregan formas de strings

--- TIPOSDATOS.py
+++ TIPOSDATOS.py
@@ -4,3 +4,20 @@ institucion = "Universidad de Los Lagos"
 asignatura = 'Programación'
 descripcion = """asignatura de primer semestre
 de la carrera de Ingeniería Civil en Informática"""
+
+
+#IMPRIME LA POSICIÓN DE UN CARACTER
+print(institucion[-2])
+
+#CONCATENACIÓN
+resultado = print(institucion + " " + asignatura)
+
+#MULTIPLICACIÓN POR TEXTO
+salida = print(institucion * 4)
+
+#UTILIZANDO LA FUNCION SPLIT
+print(institucion.split())
+
+print(asignatura.__len__())
+
+print(len(asignatura))
