c4b68f62a2a7830ea7601205cb3c9a110a6fb682
Ina <valentinacalderon466@gmail.com>
2024-05-22 16:54:27 -0400

    actividad

---
+++ Actividad.py
@@ -0,0 +1,3 @@
+articulo = "Este lápiz solo tiene formato de color azul"
+max = len(articulo) > 50
+print("Este lápiz solo tiene formato de color azul")
---
+++ Actividad2.py
@@ -0,0 +1,16 @@
+JN = (5.64, 3.45, 4.56)
+AN = (4.56, 5.64, 6.91)
+PN = (3.45, 4.01, 6.23)
+FN = (5.64, 3.45, 4.56, 4.56, 5.64, 6.91, 3.45, 4.01, 6.23)
+
+JP = (sum(JN))/3
+print("El promedio de Joel es", round(JP,3))
+AP = (sum(AN))/3
+print("El promedio de Alondra es", round(AP,3))
+PP = (sum(PN))/3
+print("El promedio de Paz es", round(JP,3))
+
+PF= float(sum(FN))/3
+
+print("El promedio de todos juntos es de", round(PF,3))
+
