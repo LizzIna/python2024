5a450a8f37874300049ecdd5dbe1581586b4d7f7
Ina <valentinacalderon466@gmail.com>
2024-05-28 22:39:16 -0400

    ejercicio casa

---
+++ Practica_casa/Ejercicio _booleanos.py
@@ -0,0 +1,19 @@
+booleano = 42 == 42
+print(booleano)
+
+
+#NEGAR UN BOOLEANO.
+b = not False
+print(b)
+
+
+bo = True and True
+print(bo)
+
+#Utilizando and
+v = 42 > 32 and 32 < 42
+print(f"es {v}")
+
+#Utilizando or
+va = 23 > 42 or 23 < 42
+print(f"es {va}")
