b05b7e90706a4a0109a7eee76f8452ec1914c442
Ina <valentinacalderon466@gmail.com>
2024-05-29 09:44:24 -0400

    practica

--- Practica_casa/Ejercicio _booleanos.py
+++ Practica_casa/Ejercicio _booleanos.py
@@ -17,3 +17,16 @@ print(f"es {v}")
 #Utilizando or
 va = 23 > 42 or 23 < 42
 print(f"es {va}")
+
+#OPERADORES LOGICOS.
+a=10
+b=15
+c=20
+resultado = ((a<b) and (b<c))
+r2 = ((a>b) and (b<c))
+r3 = ((a>b) or (b<c))
+r4 = not ((a>b) or (b<c))
+print(resultado)
+print(r2)
+print(r3)
+print(r4)
