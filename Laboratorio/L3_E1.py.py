2a8cabce20dce6cd4ad48f7809fae0fa60979c52
Ina <valentinacalderon466@gmail.com>
2024-06-14 08:54:29 -0400

    subiendo laboratorio 3

---
+++ Laboratorio/L3_E1.py.py
@@ -0,0 +1,45 @@
+#A
+id1= 14
+r1= "Los Ríos"
+s1 = 18429 
+h1 = 404432
+
+id2 = 12
+r2 = "Magallanes"
+s2 = 1382291
+h2 = 166533
+
+reg1 = {"id1": id1, "region1" : r1, "superficie1" : s1, "habitantes1": h1}
+reg2 = {"id2": id2,"region1" : r2, "superficie1" : s2, "habitantes1": h2}
+
+censo_2017 = {
+    "region1" : reg1,
+    "region2" : reg2
+}
+
+print(censo_2017)
+
+#B
+densidad1= h1/s1
+print(f"La densidad de la {id1} es de {densidad1:.1f}")
+
+densidad2= h2/s2
+print(f"La densidad de la {id2} es de {densidad2:.1f}")
+
+#C
+"""Agregar otra clave llamada “Capital” en cada sub-diccionario, correspondiente a la
+capital de cada región (ver Tabla N°2)"""
+
+reg1
+
+#D
+
+#E
+provicincia1 = ("Ranco", "valdivia")
+provincia2 = ("Antártica Chilena, Magallanes", "Tierra del Fuego", "Última Esperanza")
+#F
+
+
+#G
+print(censo_2017)
+#H
