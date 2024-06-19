55b74d31e21c0d48cfcc80bdd3e139c5d1291ca1
Ina <valentinacalderon466@gmail.com>
2024-06-19 15:35:02 -0400

    subiendo reto 5 actualizado

--- Retos/RETO5.py
+++ Retos/RETO5.py
@@ -1,3 +1,16 @@
 #RETO 5
 num = int(input("Ingrese un número al que quiera sacar su serie Fibonacci: "))
 
+a = 0
+b = 1
+
+f = [a,b]
+
+while True:
+    c = a+b
+    if c > num:
+         break
+    f.append(c)
+    a = b
+    b = c
+print(" ".join(map(str,f)))
